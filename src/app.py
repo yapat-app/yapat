e5b7fb0939b1df4fb7e9de4ac07d716d5df7c798
Thiago <thiago.gouvea@dfki.de>
2024-08-22 18:45:54 +0200

    Quasi-functional example

--- src/app.py
+++ src/app.py
@@ -3,14 +3,85 @@ import os
 
 import dash
 import dash_bootstrap_components as dbc
-from dash import html, dcc
-from flask import Flask
+from dash import html, dcc, Output, Input
+from flask import Flask, render_template, redirect, url_for, request, flash
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user
+from flask_sqlalchemy import SQLAlchemy
 
-# from components.login import User, login_location
 from components import navbar, footer
+from utils.auth import User
 from utils.settings import APP_HOST, APP_PORT, APP_DEBUG, DEV_TOOLS_PROPS_CHECK
 
+# Initialize Flask app
 server = Flask(__name__)
+server.config.update(SECRET_KEY=os.getenv('SECRET_KEY'))
+server.config.update(SQLALCHEMY_DATABASE_URI='sqlite:///users.db')
+server.config.update(SQLALCHEMY_TRACK_MODIFICATIONS=False)
+
+# Initialize Flask extensions
+db = SQLAlchemy(server)
+login_manager = LoginManager(server)
+login_manager.login_view = '/login'
+
+
+# Define User model
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    password = db.Column(db.String(80), nullable=False)
+
+
+# Create database tables
+with server.app_context():
+    db.create_all()
+
+
+# Flask-Login user loader
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+
+# Flask routes for registration and login
+@server.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        if User.query.filter_by(username=username).first():
+            flash('Username already exists.', 'danger')
+        else:
+            new_user = User(username=username, password=password)
+            db.session.add(new_user)
+            db.session.commit()
+            flash('New user created.', 'success')
+            return redirect(url_for('login'))
+    return render_template('register.html')
+
+
+# login_manager.init_app(server)
+
+@server.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        user = User.query.filter_by(username=username, password=password).first()
+        if user:
+            login_user(user, remember=True)
+            return redirect(url_for('protected'))
+        else:
+            flash('Login unsuccessful. Please check username and password', 'danger')
+    return render_template('login.html')
+
+
+@server.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('login'))
+
+
 app = dash.Dash(
     __name__,
     server=server,
@@ -18,34 +89,37 @@ app = dash.Dash(
     external_stylesheets=[
         dbc.themes.BOOTSTRAP,
         dbc.icons.FONT_AWESOME
-    ],  # fetch the proper css items we want
-    # meta_tags=[
-    #     {  # check if device is a mobile device. This is a must if you do any mobile styling
-    #         'name': 'viewport',
-    #         'content': 'width=device-width, initial-scale=1'
-    #     }
-    # ],
+    ],
     suppress_callback_exceptions=True,
     title='YAPAT | yet another PAM annotation tool'
 )
 
-server.config.update(SECRET_KEY=os.getenv('SECRET_KEY'))
+app.layout = html.Div([
+    dcc.Location(id='url', refresh=False),
+    html.Div(id='page-content')
+])
+
+@app.callback(
+    Output('page-content', 'children'),
+    Input('url', 'pathname')
+)
+def display_page(pathname):
+    if pathname == '/dash/protected':
+        return protected_layout()
+    return login_redirect_layout()
 
 
-# Login manager object will be used to login / logout users
-# login_manager = LoginManager()
-# login_manager.init_app(server)
-# login_manager.login_view = '/login'
+def login_redirect_layout():
+    return dcc.Link('Go to Login', href='/login')
 
-# @login_manager.user_loader
-# def load_user(username):
-#     """This function loads the user by user id. Typically this looks up the user from a user database.
-#     We won't be registering or looking up users in this example, since we'll just login using LDAP server.
-#     So we'll simply return a User object with the passed in username.
-#     """
-#     return User(username)
 
-def serve_layout():
+@server.route('/protected')
+@login_required
+def protected():
+    return redirect('/dash/protected')
+
+
+def protected_layout():
     '''Define the layout of the application'''
     return html.Div(
         [
@@ -68,8 +142,8 @@ def serve_layout():
     )
 
 
-app.layout = serve_layout  # set the layout to the serve_layout function
-server = app.server  # the server is needed to deploy the application
+# app.layout = serve_layout  # set the layout to the serve_layout function
+# server = app.server  # the server is needed to deploy the application
 
 if __name__ == "__main__":
     app.run_server(
