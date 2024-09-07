3c54fe61a4762c2c96cd3a8bb83e0eb5cc8c3dd2
Thiago <thiago.gouvea@dfki.de>
2024-09-07 18:29:09 +0200

    Change readthedocs theme

--- docs/conf.py
+++ docs/conf.py
@@ -14,7 +14,7 @@ release = '0.1.0-alpha'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = ['sphinx_rtd_theme']
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
