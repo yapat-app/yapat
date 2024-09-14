25bb1a0f545a742d648529eee8755d4f3371e36f
Thiago <thiago.gouvea@dfki.de>
2024-09-14 23:04:52 +0200

    Add references to DFKI

--- src/components/footer.py
+++ src/components/footer.py
@@ -5,12 +5,15 @@ layout = html.Div(
         # Footer section
         html.Footer(
             children=[
-                html.P("YAPAT Web App", style={'margin-bottom': '10px'}),
+                html.P(["© 2024 YAPAT by ", html.A("DFKI", href="https://iml.dfki.de", target="_blank"),
+                        ". Licensed under AGPL-3.0."], style={'margin-bottom': '10px'}),
                 html.A("Documentation", href="https://yapat.readthedocs.io/", target="_blank",
                        style={'margin-right': '20px'}),
                 html.A("GitHub Repository", href="https://github.com/yapat-app/yapat",
                        target="_blank", style={'margin-right': '20px'}),
-                html.A("Issue Tracker", href="https://github.com/yapat-app/yapat/issues", target="_blank"),
+                html.A("Issue Tracker", href="https://github.com/yapat-app/yapat/issues", target="_blank",
+                       style={'margin-right': '20px'}),
+                html.A("Contact", href="mailto:thiago.gouvea@dfki.de", target="_blank")
             ],
             style={
                 'position': 'fixed', 'bottom': '0', 'width': '100%', 'background-color': '#f1f1f1',
