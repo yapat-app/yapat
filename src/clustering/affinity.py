aef579e51e8acfc063fd92a0b1cf465e6462f739
rida saghir <ridasaghir@ridas-MBP.fritz.box>
2024-09-22 18:21:28 +0200

    affinity clusterer

---
+++ src/clustering/affinity.py
@@ -0,0 +1,41 @@
+from sklearn.cluster import AffinityPropagation
+import pandas as pd
+from sklearn.preprocessing import StandardScaler
+
+from clustering import BaseClustering
+
+class Affinity(BaseClustering):
+    """
+    Methods:
+    --------
+    fit(data: pd.DataFrame):
+        Fit the Affinity Propagation clustering algorithm to the data.
+
+    """
+
+    def __init__(self):
+        """
+        Initialize the HDBSCANClustering class with the minimum cluster size.
+
+        :param min_cluster_size: The minimum size of clusters. Clusters smaller than this size will be treated as noise.
+        """
+        super().__init__()
+        self.clusterer = AffinityPropagation(random_state=43)
+
+    def fit(self, dataset_id: int, embedding_id: int):
+        """
+        :param data: DataFrame containing the data to be clustered.
+        :return: DataFrame containing the cluster labels assigned to the data.
+        """
+        data = self.load_data(dataset_id, embedding_id)
+        self.scaled_data = self.scale_data(data)
+        self.clusterer.fit(self.scaled_data)
+        self.labels = pd.Series(self.clusterer.labels_, index=data.index, name='Cluster_Label')
+        #self.save_labels()
+        return self.labels
+
+clustering = Affinity()
+clustering.fit(1, 2)
+
+
+
