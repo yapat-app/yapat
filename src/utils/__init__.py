5182bbd3555d1ce9fc457fb8252d50c8451905d9
Thiago <thiago.gouvea@dfki.de>
2024-09-23 18:20:17 +0200

    Moved function definition out of __init__.py

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -1,7 +1,2 @@
-import socket
-from contextlib import closing
-
-
-def check_socket(host, port):
-    with closing(socket.socket(socket.AF_INET, socket.SOCK_STREAM)) as sock:
-        return sock.connect_ex((host, port)) == 0
+from utils.extensions import check_socket
+# from utils.embeddings import get_embedding_model
