4799e572a5149aee697a9ad78f90127f6771e48d
rida saghir <ridasaghir@ridas-MacBook-Pro.local>
2024-09-23 15:49:59 +0200

    variance and entropy in single class. classifier to be discussed for meta data

--- src/evaluations/__init__.py
+++ src/evaluations/__init__.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import json
 
 import dask
 from sklearn.preprocessing import StandardScaler
@@ -26,6 +27,7 @@ class BaseEvaluation:
             ).one_or_none()
             file_path =  embedding_result.file_path
 
+
             if file_path.endswith('.csv'):
                 self.data = pd.read_csv(file_path, index_col=0)
             elif file_path.endswith('.pkl'):
@@ -39,19 +41,23 @@ class BaseEvaluation:
         scaled_data = scaler.fit_transform(data.values)
         return scaled_data
 
-    def save_result(self, evaluation_result):
-        pass
+    def save_results(self, evaluation_results, dataset_id: int, embedding_id: int):
+        """
+        Save the evaluation results to a JSON file.
+        Parameters:
+        - results: dict, the results to be saved.
+        - dataset_id : dataset_id value in the EmbeddingResult table
+        - embedding_id : embedding_id value in the EmbeddingResult table
+        """
+        result = sqlalchemy_db.query(EmbeddingResult).filter_by(dataset_id=dataset_id, embedding_id=embedding_id).first()
+        result.evaluation_results = json.dumps(evaluation_results)
+        sqlalchemy_db.session.commit()
+        sqlalchemy_db.session.close()
 
 
-def get_evaluation_method(method_name: str, dask_client: dask.distributed.client.Client or None = None):
-    if method_name == "entropy":
-        from evaluations.entropy_evaluation import EntropyEvaluation
-        return EntropyEvaluation()
-    elif method_name == "explained_variance":
-        from evaluations.explained_variance_evaluation import ExplainedVariance
-        return ExplainedVariance()
-    else:
-        raise ValueError(f"Unknown Evaluation Method: {method_name}")
+
+
+
 
 
 
--- src/evaluations/embedding_evaluation.py
+++ src/evaluations/embedding_evaluation.py
@@ -1,21 +1,43 @@
 import numpy as np
 import pandas as pd
+
 from sklearn.decomposition import PCA
+from sklearn.svm import SVC
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import precision_score, recall_score, f1_score, accuracy_score
 from npeet.entropy_estimators import entropy as calculate_entropy
 
 from evaluations import BaseEvaluation
 
 
 class EmbeddingsEvaluation(BaseEvaluation):
-    def __init__(self, variance_threshold: float = 0.95):
+    def __init__(self, variance_threshold: float = 0.95, to_predict:str = 'Hour', max_iter:int = 50000, C: float = 0.1,
+                 gamma:float = 0.01, kernel:str = 'linear' ):
         """
-        Initialize the EmbeddingsEvaluation with a variance threshold for PCA.
-        Parameters:
-        - variance_threshold: float, the cumulative variance threshold to determine
-                              the percentage of principal components to retain.
-        """
+                Initialize the EmbeddingsEvaluation with parameters for PCA and SVC.
+                Parameters:
+                - variance_threshold: float, the cumulative variance threshold for PCA.
+                - max_iter: int, maximum iterations for the SVC.
+                - C: float, regularization parameter for the SVC.
+                - gamma: float, kernel coefficient for the SVC.
+                - kernel: str, type of kernel to be used in SVC.
+                """
         super().__init__()
-        self.variance_threshold = variance_threshold
+        self.variance_threshold = variance_threshold # For explained variance
+        self.to_predict = to_predict
+        self.model = SVC(random_state=42, max_iter=max_iter, C=C, gamma=gamma, kernel=kernel) # For classifier
+
+    def evaluate_classifier(self, X, y):
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+        self.model.fit(X_train, y_train)
+        y_pred = self.model.predict(X_test)
+        results = {
+            'accuracy': accuracy_score(y_test, y_pred),
+            'f1_score': f1_score(y_test, y_pred, average='weighted'),
+            'recall': recall_score(y_test, y_pred, average='weighted'),
+            'precision': precision_score(y_test, y_pred, average='weighted')
+        }
+        return results
 
     def calculate_entropy(self, data):
         return calculate_entropy(data)
@@ -29,16 +51,18 @@ class EmbeddingsEvaluation(BaseEvaluation):
         percentage_of_components = (num_components / len(pca_data)) * 100
         return percentage_of_components
 
+
     def evaluate(self, dataset_id: int, embedding_id: int):
 
         data = self.load_data(dataset_id, embedding_id)
         self.scaled_data = self.scale_data(data)
         entropy_result = self.calculate_entropy(self.scaled_data)
         explained_variance_result = self.calculate_explained_variance(self.scaled_data)
+        #classifier_results = self.evaluate_classifier(self.scaled_data, y)
         results = {
             "Entropy": entropy_result,
-            "Explained Variance": explained_variance_result
+            "Explained Variance": explained_variance_result,
+           # **classifier_results
         }
-        # Will work on it later
         # self.save_results()
         return results
