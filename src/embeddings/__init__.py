c661e4e6b05a6c1e46c6f33dde3335025a07143a
Thiago <thiago.gouvea@dfki.de>
2024-09-23 18:11:26 +0200

    Improve on get_embedding_model: pass args and kwargs on to each embedding model; add documentation

--- src/embeddings/__init__.py
+++ src/embeddings/__init__.py
@@ -211,15 +211,33 @@ def compute_embeddings(dataset_name: str, embedding_method: str, flask_server):
     return embedding.fit_transform(path_audio)
 
 
-def get_embedding_model(method_name: str, dask_client: dask.distributed.client.Client or None = None):
+def get_embedding_model(method_name, *args, **kwargs):
+    """
+    Retrieve the appropriate embedding model based on the given method name.
+
+    Parameters:
+    - method_name (str): The name of the embedding method to use.
+    - *args: Additional positional arguments to pass to the embedding model.
+    - **kwargs: Additional keyword arguments to pass to the embedding model.
+
+    Returns:
+    - An instance of the specified embedding model.
+
+    Raises:
+    - ValueError: If the provided method_name is not recognized.
+    """
     if method_name == "birdnet":
         from embeddings.birdnet import BirdnetEmbedding
-        return BirdnetEmbedding(method_name, dask_client)
+        return BirdnetEmbedding(*args, **kwargs)
+
     elif method_name == "acoustic_indices":
         from embeddings.acoustic_indices import AcousticIndices
-        return AcousticIndices(method_name, dask_client)
+        return AcousticIndices(*args, **kwargs)
+
     elif method_name == "vae":
         from embeddings.vae import VAEEmbedding
-        return VAEEmbedding(method_name, dask_client)
+        return VAEEmbedding(*args, **kwargs)
+
     else:
         raise ValueError(f"Unknown embedding method: {method_name}")
+
