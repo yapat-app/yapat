2531877c85e7769bad8c65be82b084f2777213f2
rida saghir <ridasaghir@ridas-MBP.fritz.box>
2024-09-25 11:24:00 +0200

    initializing the base class from main class. Creating unique file name to save results

--- src/clustering/kmeans.py
+++ src/clustering/kmeans.py
@@ -13,26 +13,28 @@ class KMeansClustering(BaseClustering):
 
     """
 
-    def __init__(self, n_clusters: int = 8):
+    def __init__(self, dataset_name, embedding_method, n_clusters: int = 8):
         """
         Initialize the KMeansClustering class with the minimum cluster size.
 
         :param min_cluster_size: The minimum size of clusters. Clusters smaller than this size will be treated as noise.
         """
-        super().__init__()
+        super().__init__(dataset_name, embedding_method)
         self.clusterer = KMeans(random_state=42, n_clusters=n_clusters)
 
-    def fit(self, dataset_id: int, embedding_id: int):
+    def fit(self):
         """
         :param data: DataFrame containing the data to be clustered.
         :return: DataFrame containing the cluster labels assigned to the data.
         """
-        data = self.load_data(dataset_id, embedding_id)
+        data = self.load_data()
         self.scaled_data = self.scale_data(data)
         self.clusterer.fit(self.scaled_data)
         self.labels = pd.DataFrame(self.clusterer.labels_, columns=['Cluster Label'], index=data.index)
-        #self.save_labels()
+        self.save_labels(,self.labels)
         return self.labels
+
+
 
 
 
--- src/clustering/optics.py
+++ src/clustering/optics.py
@@ -13,24 +13,24 @@ class OpticsClustering(BaseClustering):
 
     """
 
-    def __init__(self):
+    def __init__(self, dataset_name, embedding_method):
         """
         Initialize the OpticsClustering class with the minimum cluster size.
 
         """
-        super().__init__()
+        super().__init__(dataset_name, embedding_method)
         self.clusterer = OPTICS() # min_samples default = 5
 
-    def fit(self, dataset_id: int, embedding_id: int):
+    def fit(self):
         """
         :param data: DataFrame containing the data to be clustered.
         :return: DataFrame containing the cluster labels assigned to the data.
         """
-        data = self.load_data(dataset_id, embedding_id)
+        data = self.load_data()
         self.scaled_data = self.scale_data(data)
         self.clusterer.fit(self.scaled_data)
         self.labels = pd.DataFrame(self.clusterer.labels_, columns=['Cluster Label'], index=data.index)
-        #self.save_labels()
+        self.save_labels(self.labels)
         return self.labels
 
 
