7de9e62ed758e19fcf1229e0c3e32ece5bf472aa
rida saghir <ridasaghir@ridas-MBP.fritz.box>
2024-09-27 12:08:33 +0200

    retrieves embeddings and saves to location according to schema

--- src/clustering/__init__.py
+++ src/clustering/__init__.py
@@ -3,12 +3,14 @@ import os
 import pandas as pd
 from sklearn.preprocessing import StandardScaler
 import uuid
-
+import pickle
+import logging
 from sqlalchemy import and_
+from src import server, sqlalchemy_db
+from sqlalchemy.exc import SQLAlchemyError
+from schema_model import Dataset, EmbeddingResult, ClusteringResult, ClusteringMethod, EmbeddingMethod
 
-from extensions import sqlalchemy_db
-from schema_model import Dataset, EmbeddingMethod, EmbeddingResult, ClusteringResult
-
+logger = logging.getLogger(__name__)
 
 class BaseClustering:
     """
@@ -35,64 +37,117 @@ class BaseClustering:
 
     """
 
-    def __init__(self, dataset_name, embedding_method) -> None:
+    def __init__(self) -> None:
         """
         Initialize the BaseClustering class. Sets up the data and labels attributes.
         """
-        self.dataset_name = dataset_name
-        self.embedding_method = embedding_method
+
         self.data = None  # Will hold the data to be clustered.
         self.labels = None  # Will store the cluster labels after fitting the model.
 
-    def load_data(self) -> pd.DataFrame:
-        """
-        Load the data to be clustered from a CSV or pickle file.
+    def load_data(self, embedding_method_name):
+        session = sqlalchemy_db.session
+        try:
+            with server.app_context():
+                selected_dataset = session.query(Dataset).filter_by(is_selected=True).first()
+                if not selected_dataset:
+                    logger.error("No dataset is currently selected.")
 
-        :param file_path: Path to the data file (CSV or pickle format).
-        :return: DataFrame containing the loaded data.
-        """
-        self.dataset_id = sqlalchemy_db.session.execute(
-            sqlalchemy_db.select(Dataset.id).where(Dataset.dataset_name == self.dataset_name)
-        ).scalar_one_or_none()
-        self.embedding_id = sqlalchemy_db.session.execute(
-            sqlalchemy_db.select(EmbeddingMethod.id).where(EmbeddingMethod.method_name == self.embedding_method)
-        ).scalar_one_or_none()
-        if self.dataset_id is None or self.embedding_id is None:
-            return None
+                embedding_method = session.query(EmbeddingMethod).filter_by(method_name=embedding_method_name).first()
+                if not embedding_method:
+                    logger.error(f"No embedding method found for '{embedding_method_name}'.")
 
-        file_path = sqlalchemy_db.session.execute(
-            sqlalchemy_db.select(EmbeddingResult.file_path).where(and_(EmbeddingResult.dataset_id == self.dataset_id,
-                                                                       EmbeddingResult.embedding_id == self.embedding_id))
-        )
+                embedding_result = session.query(EmbeddingResult).filter_by(dataset_id=selected_dataset.id,
+                                                                            embedding_id=embedding_method.id).first()
+                if not embedding_result:
+                    logger.error(f"No embedding result found for dataset '{selected_dataset.dataset_name}'.")
 
-        file_path = '/Users/ridasaghir/Desktop/exp/anura/anura_encodings.pkl'
-        if file_path.endswith('.csv'):
-            self.data = pd.read_csv(file_path, index_col=0)
-        elif file_path.endswith('.pkl'):
-            self.data = pd.read_pickle(file_path)
-        else:
-            raise ValueError(f"Unsupported file type: {file_path}. Please use CSV or pickle.")
-        return self.data
+                # Check if the file path exists
+                embedding_file_path = embedding_result.file_path
+                if not os.path.exists(embedding_file_path):
+                    logger.error(f"Embedding result file does not exist at path: {embedding_file_path}")
+                    raise FileNotFoundError(f"Embedding result file does not exist at path: {embedding_file_path}")
+
+                embedding_data_df = pd.read_pickle(embedding_file_path)
+                return embedding_data_df
+
+        except SQLAlchemyError as e:
+            session.rollback()
+            logger.error(f"Database error: {e}")
+            raise Exception(f"Database error: {e}")
+
+        except (ValueError, FileNotFoundError) as e:
+            raise e  # Re-raise to handle it outside if needed
+
+        except Exception as e:
+            logger.error(f"Error loading embedding data: {e}")
+            raise Exception(f"Error loading embedding data: {e}")
+
 
     def scale_data(self, data):
+        data.dropna(axis=1, inplace=True)
         scaler = StandardScaler()
         scaled_data = scaler.fit_transform(data.values)
         return scaled_data
 
-    def save_labels(self, labels):
-        """
-        Save the cluster labels to a CSV or pickle file.
+    def save_labels(self, clustering_method_name: str, embedding_method_name: str, labels: pd.DataFrame):
+        session = sqlalchemy_db.session
+        try:
+            with server.app_context():
+                selected_dataset = session.query(Dataset).filter_by(is_selected=True).first()
+                if not selected_dataset:
+                    raise ValueError("No dataset is currently selected.")
+                embedding_method = session.query(EmbeddingMethod).filter_by(method_name=embedding_method_name).first()
+                if not embedding_method:
+                    raise ValueError(f"No embedding method found for '{embedding_method_name}'.")
 
-        :param file_path: Path where the cluster labels will be saved.
-        """
-        self.labels = labels
-        if self.labels is None:
-            raise ValueError("No cluster labels found. Fit the model or predict labels first.")
+                embedding_result = session.query(EmbeddingResult).filter_by(
+                    dataset_id=selected_dataset.id,
+                    embedding_id=embedding_method.id
+                ).first()
+                if not embedding_result:
+                    raise ValueError(
+                        f"No embedding result found for dataset '{selected_dataset.dataset_name}' using the '{embedding_method_name}' method.")
 
-        unique_filename = f"{uuid.uuid4().hex}.pkl"
-        file_path = os.path.join('results/', unique_filename)
-        self.labels.to_pickle(file_path)
-        # SQL QUERY TO SAVE RESULT
+                clustering_method = session.query(ClusteringMethod).filter_by(
+                    method_name=clustering_method_name).first()
+                if not clustering_method:
+                    raise ValueError(f"No clustering method found for '{clustering_method_name}'.")
+
+                # Generate a unique file name and save the clustering labels to a pickle file
+                unique_filename = f"{uuid.uuid4().hex}_clustering.pkl"
+                clustering_file_path = os.path.join('results/', unique_filename)
+                labels.to_pickle(clustering_file_path)
+
+                # Check if a ClusteringResult already exists for this embedding and clustering method
+                clustering_result = session.query(ClusteringResult).filter_by(
+                    embedding_id=embedding_result.id,
+                    method_id=clustering_method.method_id
+                ).first()
+
+                if not clustering_result:
+                    clustering_result = ClusteringResult(
+                        embedding_id=embedding_result.id,
+                        method_id=clustering_method.method_id,
+                        cluster_file_path=clustering_file_path,
+                        hyperparameters={},  # Add relevant hyperparameters if any
+                        evaluation_results={},  # Add evaluation results if any
+                    )
+                    session.add(clustering_result)
+                else:
+                    # Update the existing ClusteringResult with the new file path
+                    clustering_result.cluster_file_path = clustering_file_path
+                    clustering_result.task = 'completed'
+                session.commit()
+                print(f"Clustering labels saved successfully at {clustering_file_path}")
+
+        except SQLAlchemyError as e:
+            session.rollback()
+            logger.error(f"Database error: {e}")
+            raise Exception(f"Database error: {e}")
+        except Exception as e:
+            logger.error(f"Error saving clustering labels: {e}")
+            raise Exception(f"Error saving clustering labels: {e}")
 
 
 
@@ -116,4 +171,4 @@ def get_clustering_model(method_name: str, *args, **kwargs):
         from clustering.optics import OpticsClustering
         return OpticsClustering(*args, **kwargs)
     else:
-        raise ValueError(f"Unknown Clustering method: {method_name}")
+        print(f"Unknown Clustering method: {method_name}")
