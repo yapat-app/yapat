d03a388bf622a7202d302495568840ac10a603b5
rida saghir <ridasaghir@ridas-MBP.fritz.box>
2024-09-29 14:34:33 +0200

    evaluations are performed every time a pipeline is selected

--- src/evaluations/clustering_evaluation.py
+++ src/evaluations/clustering_evaluation.py
@@ -4,11 +4,11 @@ from sklearn.cluster import DBSCAN
 from evaluations import BaseEvaluation
 
 class ClusteringEvaluation(BaseEvaluation):
-    def __init__(self):
+    def __init__(self, embedding_method, clustering_method):
         """
                 Initialize the ClusteringEvaluation.
         """
-        super().__init__()
+        super().__init__(embedding_method, clustering_method)
 
     def sil_score(self, embeddings, cluster_labels):
         return silhouette_score(embeddings, cluster_labels) # Silhouette Score
@@ -16,18 +16,17 @@ class ClusteringEvaluation(BaseEvaluation):
     def db_score(self, embeddings, cluster_labels): # Davies Bouldin Score
         return davies_bouldin_score(embeddings, cluster_labels)
 
-    def evaluate(self, dataset_id: int, embedding_id: int):
-        # using dataset_id and embedding_id assuming that evaluation would be called for every clustering result
-        data = self.load_data('embeddings', dataset_id, embedding_id)
-        embedding_columns = [col for col in data.columns if col != 'Cluster Label']
-        sil_score = self.sil_score(data[embedding_columns], data['Cluster Label'])
-        db_score = self.db_score(data[embedding_columns], data['Cluster Label'])
+    def evaluate(self):
+        embeddings, cluster_labels = self.load_data()
+        self.scaled_data = self.scale_data(embeddings)
+        sil_score = self.sil_score(self.scaled_data, cluster_labels)
+        db_score = self.db_score(self.scaled_data, cluster_labels)
         evaluation_results = {
             "Silhouette Score": sil_score,
             "Davies Bouldin Score": db_score
         }
-        #self.save_results('clustering', evaluation_results, dataset_id, embedding_id)
-        return evaluation_results
+        self.save_results('clusters', evaluation_results)
+        return
 
 
 
